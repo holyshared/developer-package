cee11d48230f7a277ec9b2eac5c8785da3a6b2ea
Noritaka Horio <holy.shared.design@gmail.com>
2010-05-30 01:54:46 +0900

    The package electronic alias is corrected from mootools-plugin-builder to mootools_plugin_builder.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- packages/mootools-plugin-builder/blocks/builder/view.php
+++
@@ -1,5 +0,0 @@
-<?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
-<div id="ccm-SlideshowBlock-display<?php echo intval($bID)?>" class="ccm-SlideshowBlock-display">
-<div id="ccm-SlideshowBlock-heightSetter<?php echo intval($bID)?>" class="ccm-SlideshowBlock-heightSetter"></div>
-<div class="ccm-SlideshowBlock-clear" ></div>
-</div>
