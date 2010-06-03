1b2d9f1e70ca78972e1d726c42dcc8d74f004587
holy <holy.shared.design@gmail.com>
2010-06-03 18:42:40 +0900

    add template plugin templates.
    Signed-off-by: holy <holy.shared.design@gmail.com>

---
+++ packages/mootools_plugin_builder/blocks/content/lists/template_list.js
@@ -0,0 +1,6 @@
+var tinyMCETemplateList = [
+	["Module", "tinymce/templates/module_default.htm", "Module Default"],
+	["2Columns", "tinymce/templates/grid_2columns(50%).htm", "2Columns"],
+	["3Columns", "tinymce/templates/grid_3columns(33%).htm", "3Columns"],
+	["5Columns", "tinymce/templates/grid_5columns(20%).htm", "5Columns"]
+];
--- packages/mootools_plugin_builder/blocks/content/templates/line.php
+++ packages/mootools_plugin_builder/blocks/content/templates/line.php
@@ -2,8 +2,6 @@
 	defined('C5_EXECUTE') or die(_("Access Denied."));
 	$content = $controller->getContent();
 ?>
-<div class="mod">
-	<div class="inner">
-		<?php print $content; ?>
-	</div>
+<div class="line">
+	<?php print $content; ?>
 </div>
