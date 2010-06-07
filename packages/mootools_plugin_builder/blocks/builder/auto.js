687e0b7acc05c287718ef1f28d3f07badffcbdfe
holy <holy.shared.design@gmail.com>
2010-06-07 13:10:08 +0900

    add form test files.
    Signed-off-by: holy <holy.shared.design@gmail.com>

--- packages/mootools_plugin_builder/blocks/builder/auto.js
+++ packages/mootools_plugin_builder/blocks/builder/auto.js
@@ -1,13 +1,11 @@
 $(function() {
 
-	
-
 	var PackageList = {
-	
+
 		initialize: function(list) {
 			this.list = list;
 		},
-		
+
 		add: function(key, value) {
 			var li = $("<li/>").attr("id", "package-" + key);
 			var strong = $("<strong/>").html(value);
@@ -17,12 +15,13 @@ $(function() {
 
 		remove: function(row){
 		}
-		
+
 	};
 
 	this.PackageSelecter = {
-	
+
 		initialize: function(pulldown, button){
+			this.noSelectPackage = false;
 			this.pulldown = pulldown;
 			this.button = button;
 			this.items = $(this.pulldown).children('option');
@@ -31,8 +30,20 @@ $(function() {
 
 		addPackage: function(event) {
 			event.preventDefault();
-			var item = $(this.pulldown).val();
-			$.proxy(PackageList.add, PackageList)(item, PluginPackages[item]);
+			if (!this.noSelectPackage) {
+				var items = $(this.pulldown).children('option');
+				var item = $(this.pulldown).val();
+				var curent = $(this.pulldown).find('option[value=' + item + ']');
+				$.proxy(PackageList.add, PackageList)(item, PluginPackages[item]);
+				curent.remove();
+	
+				if (items.length <= 1) {
+					var option = $("<option/>").val("").html("not package");
+					$(this.pulldown).append(option);
+					$(this.pulldown).attr("disable", "disable");
+					this.noSelectPackage = true;
+				}
+			}
 		}
 	};
 	PackageList.initialize.apply(PackageList, [$("#packageList")]);
