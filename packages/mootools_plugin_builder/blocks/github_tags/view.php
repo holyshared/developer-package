7a8ef58c2992f6fdccf4e3060c45b27f8805ae12
Noritaka Horio <holy.shared.design@gmail.com>
2010-06-25 00:36:19 +0900

    github template changed.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- packages/mootools_plugin_builder/blocks/github_tags/view.php
+++ packages/mootools_plugin_builder/blocks/github_tags/view.php
@@ -1,18 +1,21 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
 <?php $t = Loader::helper('text'); ?>
-<div class="mod">
+<div class="mod corner aside">
 	<div class="inner">
-		<div class="hd"><h3><?php echo $t->entities($title); ?></h3></div>
+		<div class="hd download">
+			<h3 class="h4"><?php echo $t->entities($title); ?></h3>
+		</div>
 		<div class="bd">
-			<ul>
-				<?php foreach($tags as $key => $tag) : ?>
-				<?php
-					$baseURL = "http://github.com/holyshared/MMap/";
-					$tgz = $baseURL."tarball/".$key;
-					$zip = $baseURL."zipball/".$key;
-				?>
-					<li>version <?php echo $key ?><br /><a title="<?php echo $key; ?> download(.tgz)" href="<?php echo $tgz; ?>">tgz</a> | <a title="<?php echo $key; ?> download(.zip)" href="<?php echo $zip; ?>">zip</a></li>
-				<?php endforeach; ?>
+			<ul class="nav separator versionList">
+<?php foreach($tags as $key => $tag) : ?>
+	<?php
+		$baseURL = "http://github.com/holyshared/MMap/";
+		$tgz = $baseURL."tarball/".$key;
+		$zip = $baseURL."zipball/".$key;
+	?>
+	<li><em><?php echo $key ?></em>&nbsp;<span class="aFiles"><a href="<?php echo $tgz ?>">tgz file</a> | <a href="<?php echo $zip ?>">zip file</a></span></li>
+<?php endforeach; ?>
+
 			</ul>
 		</div>
 	</div>
