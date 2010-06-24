e2c0b4b357edec53237b8cd7288127754cbc5fdf
Noritaka Horio <holy.shared.design@gmail.com>
2010-06-25 02:00:43 +0900

    add github issuse block.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

---
+++ packages/mootools_plugin_builder/blocks/github_issues/auto.js
@@ -0,0 +1,34 @@
+var GithubTags = {};
+
+GithubTags.Validater = {
+
+	validate: function() {
+		var failed = false;
+
+		var formTitle = $('#title');
+		if (formTitle.val() == '') {
+			alert(ccm_t('title'));
+			failed = true;
+		}
+	
+		var downloadFile = $('#repos');
+		if (downloadFile.val() == '') {
+			alert(ccm_t('repos'));
+			failed = true;
+		}
+
+		var displayCount = $('#displayCount');
+		if (displayCount.val() == '') {
+			alert(ccm_t('display-count'));
+			failed = true;
+		}
+
+		if (failed) {
+			ccm_isBlockError = 1;
+			return false;
+		}
+		return true;
+	}
+};
+
+ccmValidateBlockForm = function() { return GithubTags.Validater.validate(); }
---
+++ packages/mootools_plugin_builder/blocks/github_issues/controller.php
@@ -0,0 +1,89 @@
+<?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
+<?php
+
+class GithubIssuesBlockController extends BlockController {
+
+	protected $btTable = 'btGithubIssues';
+	protected $btInterfaceWidth = "550";
+	protected $btInterfaceHeight = "400";
+
+	public function getBlockTypeDescription() {
+		return t("Tag list of github repository");
+	}
+
+	public function getBlockTypeName() {
+		return t("Github Issues");
+	}
+
+	public function getJavaScriptStrings() {
+		return array(
+			"title"	=> "Please input a title.",
+			"repos"	=> "Please select the repository.",
+			"display-count"	=> "Please select the display number."
+		);
+	}
+
+	public function view() {
+		$this->set("tags", $this->getUserRepositoryTags());
+	}
+
+	public function add() {
+		$u = new User();
+		$ui = UserInfo::getByID($u->getUserID());
+		$userName = $ui->getAttribute(MOOTOOLS_GITHUB_USER);
+
+		$this->set("userName", $userName);
+		$this->set("repositories", $this->getUserRepositories());
+	}
+
+	public function edit() {
+		$u = new User();
+		$ui = UserInfo::getByID($u->getUserID());
+		$userName = $ui->getAttribute(MOOTOOLS_GITHUB_USER);
+
+		$this->set("userName", $userName);
+		$this->set("repositories", $this->getUserRepositories());
+	}
+
+	public function delete() {
+		parent::delete();
+	}
+
+	public function save($data) {
+		parent::save($data);
+	}
+
+	protected function getUserRepositories() {
+		Loader::library("3rdparty/github/phpGitHubApi", MootoolsPluginBuilderPackage::PACKAGE_HANDLE);
+
+		$u = new User();
+		$ui = UserInfo::getByID($u->getUserID());
+		$userName = $ui->getAttribute(MOOTOOLS_GITHUB_USER);
+
+		$github = new phpGitHubApi();
+		$api = $github->getRepoApi();
+		$repositories = $api->getUserRepos($userName);
+
+		return $repositories;
+	}
+
+	protected function getUserRepositoryTags() {
+		Loader::library("3rdparty/github/phpGitHubApi", MootoolsPluginBuilderPackage::PACKAGE_HANDLE);
+
+		$tags = array();
+
+		$u = new User();
+		$ui = UserInfo::getByID($u->getUserID());
+		$userName = $ui->getAttribute(MOOTOOLS_GITHUB_USER);
+
+		if (!empty($userName)) {
+			$github = new phpGitHubApi();
+			$api = $github->getRepoApi();
+			$tags = $api->getRepoTags($userName, $this->repos);
+		}
+		return $tags;
+	}
+	
+}
+
+?>
