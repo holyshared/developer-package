381ba34017066507c75facd07ff76f95f70249f8
holy <holy.shared.design@gmail.com>
2010-07-01 12:55:21 +0900

    css template changed.
    Signed-off-by: holy <holy.shared.design@gmail.com>

--- packages/mootools_plugin_builder/blocks/github_issues/templates/list.php
+++ packages/mootools_plugin_builder/blocks/github_issues/templates/list.php
@@ -4,15 +4,12 @@
 
 	<div class="inner">
 		<div class="hd">
-			<h3 class="h4"><?php echo $t->entities($title); ?></h3>
+			<h3><?php echo $t->entities($title); ?></h3>
 		</div>
 		<div class="bd">
 
-
-
-
-
 <?php
+
 foreach($issues as $key => $issue) :
 
 $url  = "http://www.gravatar.com/avatar/";
@@ -23,6 +20,7 @@ $ticket  = "http://github.com/".$t->entities($user)."/".$t->entities($repos);
 $ticket .= "/issues#issue/".$t->entities($issue["position"]);
 
 $updateAt = date("Y-m-d H:i", strtotime($issue["updated_at"]));
+$createAt = date("Y-m-d H:i", strtotime($issue["created_at"]));
 
 ?>
 
@@ -30,11 +28,10 @@ $updateAt = date("Y-m-d H:i", strtotime($issue["updated_at"]));
 	<p class="avater"><a href="http://github.com/<?php echo $t->entities($user) ?>/"><img src="<?php echo $t->entities($url) ?>" alt="<?php echo $t->entities($user) ?>" class="image" /></a></p>
 	<h4><a href="<?php echo $ticket ?>"><?php echo $t->entities($issue["title"]) ?></a></h4>
 	<p><?php echo $t->entities($issue["body"]) ?></p>
-	<p>update at: <?php echo $t->entities($updateAt) ?></p>
+	<p>update at: <?php echo $t->entities($updateAt) ?> | create at: <?php echo $t->entities($createAt) ?> | user: <a href="http://github.com/<?php echo $t->entities($user) ?>/"><?php echo $t->entities($issue["user"]) ?></a></p>
 </div>
 
 <?php endforeach; ?>
-
 
 		</div>
 	</div>
